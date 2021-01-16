83319f1206c921bf11ef38481cd44d2265e6edc5
Jonathan01-CMD <77316408+Jonathan01-CMD@users.noreply.github.com>
2021-01-16 18:47:32 -0400

    Add files via upload

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jonathan Jose Hidalgo de Jesus!");
+            Console.WriteLine("Esta es la segunda parte del primer ejercicio");
+        }
+    }
+}
---
+++ Ejercico3/Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Dijite numero:");
+            numero = int.Parse(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Numero es positivo");
+            }
+            else
+            {
+                Console.WriteLine("Numero ees Negativo");
+            }
+        }
+    }
+}
